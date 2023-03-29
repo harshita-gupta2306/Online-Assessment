0a585f3346ce2a38cb17705563848d336f8d21ae
unknown <harsita2001@gmail.com>
2023-03-29 20:56:12 +0530

    combined the components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Form from 'react-bootstrap/Form'
 import  Button  from 'react-bootstrap/Button'
-import Sign_img from './Sign_img'
+import { useRef } from 'react';
+import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
-
+import Webcam from 'react-webcam';
 
 
 const Home = () => {
+    const webRef = useRef(null);
+    const [url, setUrl] = useState(null);
+
+    const capturePhoto = React.useCallback(async () => {
+        const imageSrc = webRef.current.getScreenshot()
+        setUrl(imageSrc)
+    }, [webRef])
+
+    const onUserMedia = (e) => {
+        console.log(e)
+    }
     const [inpval,setInpval] =useState({
         name:"",
         lastName:"",
@@ -104,7 +116,43 @@ const Home = () => {
                         </Card>
                     </div>
 
-                    <Sign_img></Sign_img>  
+                    <div className='right-data mt-4 mb-5'>
+                {/* <div className='sign_img mt-5' >
+                    <img src="./sign.svg" style={{ maxWidth: 400, height: 400 }} alt="" />
+                </div> */}
+                <Card style={{ width: '23rem', height: '60rem' }}>
+                    {/* <Card.Img variant="top" src="./user2.svg" /> */}
+                    <Webcam
+                        ref={webRef}
+                        screenshotFormat="image/png"
+                        onUserMedia={onUserMedia}
+                    />
+                    {/* <img  src="./new.avif" style={{ maxWidth: 300, height: 300, border:"rgb(20,20,20)", marginLeft:'2rem',marginTop:'1rem',marginRight:'2rem' }} alt="" /> */}
+                    <Card.Body>
+                        <Card.Title>Photo Authentication</Card.Title>
+                        <Card.Text>
+                            Please click your photo using the below button for Authentication purpose
+                        </Card.Text>
+                        <Button className='col-lg-12' variant="success" style={{ border: "rgb(67,185,127)", justifySelf: 'center', marginBottom: '1rem' }} onClick={capturePhoto}>Click Image</Button>
+                        <Card.Text>
+                            You Can view Your photo after clicking the image
+                        </Card.Text>
+                    </Card.Body>
+                    <Card style={{ width: '22rem', height: '16.5rem', background: 'rgb(141, 143, 148)', margin: '0.5rem' }}>
+                        {url && (
+                            <Card.Img variant="top" src={url} alt="ScreenShot" />
+
+                        )}
+                    </Card>
+                    <Card.Body>
+
+                        <Card.Text>
+                            If you want Clear the photo and take another photo please click the below Clear Image Button
+                        </Card.Text>
+                        <Button className='col-lg-12' variant="success" onClick={() => setUrl(null)}>Clear Image</Button>
+                    </Card.Body>
+                </Card>
+            </div>  
                 </section>
 
             </div>
