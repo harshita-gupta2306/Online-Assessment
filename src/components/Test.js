adcf170b892639837fb29cb30814efb7a97eb74c
shanthan reddy <shanthanreddyannedi26@gmail.com>
2023-04-08 09:34:42 +0530

    updated timer

--- src/components/Test.js
+++ src/components/Test.js
@@ -3,85 +3,103 @@ import { useState } from "react";
 import Question from "./Question.js";
 import { useEffect } from "react";
 import axios from "axios";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 import { Spinner } from "react-bootstrap";
 import Webcam from "react-webcam";
 
-
 const Test = (props) => {
-  
-    const location = useLocation();
-  const { linkId } = location.state
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [markedAnswers, setMarkedAnswers] = useState([]);
-    const [questions, setQuestions] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const location = useLocation();
+  const { linkId } = location.state;
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [markedAnswers, setMarkedAnswers] = useState([]);
+  const [questions, setQuestions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-      axios
-        .get(`http://65.0.130.13:8080/api/assessmentAttempt/questions?linkId=${linkId}`)
-        .then((response) => {
-          setQuestions(response.data);
-          setMarkedAnswers(new Array(response.data.length));
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }, []);
-  
-    const submitAssessment = () => {
-      console.log(markedAnswers)
-        // Logic to submit the assessment
-      };
+  useEffect(() => {
+    axios
+      .get(
+        `http://65.0.130.13:8080/api/assessmentAttempt/questions?linkId=${linkId}`
+      )
+      .then((response) => {
+        setQuestions(response.data);
+        setMarkedAnswers(new Array(response.data.length));
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [linkId]);
 
-      const setAnswer = (index) => {
-        setMarkedAnswers((arr) => {
-          const newArr = [...arr];
-          newArr[currentQuestionIndex] = index;
-          return newArr;
-        });
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
-      };
-  
-    return (
-      <div className="quiz-screen">
-      
-        {isLoading ? (
+  const submitAssessment = () => {
+    console.log(markedAnswers); // Logic to submit the assessment
+  };
+
+  const setAnswer = (index) => {
+    setMarkedAnswers((arr) => {
+      const newArr = [...arr];
+      const qid = questions[currentQuestionIndex].id;
+      const existingAnswerIndex = newArr.findIndex(
+        (item) => item && item.qid === qid
+      );
+
+      if (existingAnswerIndex === -1) {
+        newArr[currentQuestionIndex] = {
+          qid,
+          answer: questions[currentQuestionIndex].options[index],
+        };
+      } else {
+        newArr[existingAnswerIndex] = {
+          qid,
+          answer: questions[currentQuestionIndex].options[index],
+        };
+      }
+
+      return newArr;
+    });
+    setCurrentQuestionIndex((currentQuestionIndex)=>currentQuestionIndex + 1);
+  };
+
+  return (
+    <div className="quiz-screen">
+      {isLoading ? (
         // <div className="loading">Loading...</div>
         <div
-        className="spinner-container"
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Spinner animation="border" role="status" style={{ color: "#10ccf4" }}>
-          <span className="sr-only">Loading...</span>
-        </Spinner>
-      </div>
-      ) :questions.length > 0 && currentQuestionIndex < questions.length ? (
-          <Question
-            question={questions[currentQuestionIndex]}
-            totalQuestions={questions.length}
-            currentQuestion={currentQuestionIndex + 1}
-            setAnswer={setAnswer}
-            linkId={linkId}
-          />
-        ) : (
-          <div className="assessment-completed">
+          className="spinner-container"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Spinner
+            animation="border"
+            role="status"
+            style={{ color: "#10ccf4" }}
+          >
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        </div>
+      ) : questions.length > 0 && currentQuestionIndex < questions.length ? (
+        <Question
+          question={questions[currentQuestionIndex]}
+          totalQuestions={questions.length}
+          currentQuestion={currentQuestionIndex + 1}
+          setAnswer={setAnswer}
+          linkId={linkId}
+        />
+      ) : (
+        <div className="assessment-completed">
           <h2>Assessment Completed</h2>
           <button onClick={submitAssessment}>Submit</button>
         </div>
-        )}
-      </div>
-    );
-  };
+      )}
+    </div>
+  );
+};
 
-  export default Test;
+export default Test;
