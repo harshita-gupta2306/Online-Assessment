3375713105a560619a1872fa374a6a4d8ea67e23
shanthan reddy <shanthanreddyannedi26@gmail.com>
2023-04-10 13:18:25 +0530

    updated loading screen

--- src/components/Assessments.js
+++ src/components/Assessments.js
@@ -5,6 +5,7 @@ import axios from "axios";
 
 const Assessment = () => {
   const [objects, setObjects] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
@@ -13,10 +14,22 @@ const Assessment = () => {
       .then(data => {
         console.log(data);
         setObjects(data)
+        setLoading(false);
       }).catch((e)=>{
         console.log(e)
       });
   }, []);
+  
+  if (loading) {
+    return (
+      <div className="assessment-container">
+        <div className="loading-container">
+          <div className="loading-circle"></div>
+          <h2 className="loading-text">Loading...</h2>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="assessment-container">
@@ -30,8 +43,8 @@ const Assessment = () => {
             <div className="assessment-card">
               <h3 className="assessment-card-title">{object.name}</h3>
               <p className="assessment-card-description">{object.assessmentDetails}</p>
-              <p className='assessment-card-description'>Exam Starts At: {object.startDate ? object.startDate.split("T")[0] : "Not available"}</p>
-              <p className='assessment-card-description'>Exam Ends At: {object.expiryDate  ? object.expiryDate.split("T")[0] : "Not available"}</p>
+              <p className='assessment-card-description'>Exam Starts At: {object.expiryDate ? object.expiryDate.split("T")[0] : "Not available"}</p>
+              <p className='assessment-card-description'>Exam Ends At: {object.startDate  ? object.startDate.split("T")[0] : "Not available"}</p>
               <p className='assessment-card-description'>Exam Duration: {object.duration}</p>
             </div>
           </Link>
